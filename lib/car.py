2be617c9466d3e5a6a97068ceff82254cbe6b4a0
Your Name <kkamadi567@gmail.com>
2023-12-08 21:53:02 +0300

    added

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,10 @@
 from vehicle import Vehicle
 
 class Car(Vehicle):
+    def __init__(self, wheel_size, wheel_number):
+        self.wheel_size = wheel_size
+        self.wheel_number = wheel_number
+    
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
     pass
